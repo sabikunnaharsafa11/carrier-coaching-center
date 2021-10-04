3248e4e3e4080a6d36e56a5022929d19159203c0
sabikunnaharsafa11 <sabiunnaharsafa92@gmail.com>
2021-10-04 22:30:35 +0600

    total

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
+# <h1> Carrier Coaching </h1>
+
 This project was react-js & bootstrapped with https://carrier-coaching.netlify.app/
 
 What I Using For Creating This Site
 
-React JS
-BootStrap
-CSS
+*React JS
+*BootStrap
+*CSS
 Here Is My Site Details What We Can
 
-We Can Got carrier coaching information In Card
-We Can Show 3 Types Data about course by Own JSON Data
-We Can Add Them In Cart or List
-We Can Give Also Their Numeric Value
-We Got The Quantity of Card While Selecting Button
+*We Can Got carrier coaching information In Card
+*We Can Show 3 Types Data about course by Own JSON Data
+*We Can Add Them In Cart or List
+*We Can Give Also Their Numeric Value
+*We Got The Quantity of Card While Selecting Button
 My Deploy Platform
 
-Netlify
+*Netlify
 
--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -6,16 +6,16 @@ const Banner = () => {
         <div className="banner-container mt-5 ">
       <div className="">
         <div className="row d-flex banner align-items-center justify-content-center">
-          <div className="col-md-6">
+          <div className="col-md-6 mt-4 ">
          <h4>Well come To Carrier Coaching</h4>
         <h2>Carrier Coaching Is Best In Coaching Center </h2>
-            <p className="text-black text-center mt-2">
-           Welcome to our page we think you passed the some time with very very good fellings.you gain some <br/>advice and some tricky Question from this website.
+            <p className="text-black text-center mt-2 ms-5 ">
+           Welcome to our page we think you passed the some time with very very good <br/>fellings.you gain some advice and some tricky Question from this website.
             </p>
             <button className="mt-1 btn btn-danger">Read More</button>
           </div>
           <div className="col-md-6">
-          <img src="https://qph.fs.quoracdn.net/main-qimg-f6dee1b5714fceb98873e76c0a437387" class="d-block w-100" alt="..."></img>
+          <img  src="https://www.pngkey.com/png/detail/252-2523795_men-with-laptop-png.png" alt="..."></img>
           </div>
         </div>
       </div>
